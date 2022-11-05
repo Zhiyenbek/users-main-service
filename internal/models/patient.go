4dc0c68aed3d01282aaa051991a0cb0a6ebd58bf
doughnut <malika240502@gmail.com>
2022-11-05 20:55:56 +0600

    Add patient model

--- internal/models/patient.go
+++ internal/models/patient.go
@@ -1,13 +1,51 @@
 package models
 
+// import "encoding/json"
+
 type CreatePatientRequest struct {
+	BloodType        int32
+	EmergencyContact string
+	MaritalStatus    string
+	FirstName        string
+	LastName         string
+	MiddleName       string
+	BirthDate        string
+	IIN              int64
+	Phone            string
+	Address          string
+	Email            string
 }
 
 type CreatePatientResponse struct {
+	ID int64
 }
 
 type UpdatePatientRequest struct {
+	ID               int64
+	BloodType        int32
+	EmergencyContact string
+	MaritalStatus    string
+	FirstName        string
+	LastName         string
+	MiddleName       string
+	BirthDate        string
+	IIN              int64
+	Phone            string
+	Address          string
+	Email            string
 }
 
 type GetPatientResponse struct {
+	ID               int64
+	BloodType        int32
+	EmergencyContact string
+	MaritalStatus    string
+	FirstName        string
+	LastName         string
+	MiddleName       string
+	BirthDate        string
+	IIN              int64
+	Phone            string
+	Address          string
+	Email            string
 }
