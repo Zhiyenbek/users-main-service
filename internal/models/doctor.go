98f7cc8b67a6f5f03e2bfa8b4c07d4a89d4ae916
doughnut <malika240502@gmail.com>
2022-11-05 21:02:54 +0600

    Add models for patient and doctor

--- internal/models/doctor.go
+++ internal/models/doctor.go
@@ -1,12 +1,50 @@
 package models
 
 type CreateDoctorRequest struct {
+	FirstName    string
+	LastName     string
+	MiddleName   string
+	BirthDate    string
+	IIN          int64
+	Phone        string
+	Address      string
+	Email        string
+	DepartmentId int32
+	SpecId       int32
+	Experience   int32
+	Photo        string
+	Category     string
+	Price        int32
+	Schedule     string
+	Degree       string
+	Rating       int32
+	WebsiteUrl   string
 }
 
 type CreateDoctorResponse struct {
+	ID int64
 }
 
 type UpdateDoctorRequest struct {
+	ID           int64
+	FirstName    string
+	LastName     string
+	MiddleName   string
+	BirthDate    string
+	IIN          int64
+	Phone        string
+	Address      string
+	Email        string
+	DepartmentId int32
+	SpecId       int32
+	Experience   int32
+	Photo        string
+	Category     string
+	Price        int32
+	Schedule     string
+	Degree       string
+	Rating       int32
+	WebsiteUrl   string
 }
 
 type GetDoctorResponse struct {
