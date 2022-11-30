d8c4e3a681cd74b1322ae0a0a68b1bf61145518b
doughnut <malika240502@gmail.com>
2022-12-01 00:22:14 +0600

    Add database url

--- internal/repository/connection/postgres.go
+++ internal/repository/connection/postgres.go
@@ -4,14 +4,21 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Zhiyenbek/users-main-service/config"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func NewPostgresDB(cfg *config.DBConf) (*pgxpool.Pool, error) {
-	dbURI := fmt.Sprintf("postgresql://%s:%s@%s:%v/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
-	log.Println(cfg.DBName)
+	dbURI, ok := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		log.Println("Couldn't get database url. Continuing with config")
+		dbURI = fmt.Sprintf("postgresql://%s:%s@%s:%v/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+	} else {
+		log.Println("Database url: ", dbURI)
+	}
+	log.Println(dbURI)
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.TimeOut)
 	defer cancel()
 	pool, err := pgxpool.Connect(ctx, dbURI)
