7f1370763298bfd87f3ce555757ce90db5c417f0
doughnut <malika240502@gmail.com>
2022-12-04 00:59:40 +0600

    Connect to redis by heroku url

--- internal/repository/connection/redis.go
+++ internal/repository/connection/redis.go
@@ -1,6 +1,8 @@
 package connection
 
 import (
+	"log"
+	"os"
 	"strconv"
 
 	"github.com/Zhiyenbek/users-main-service/config"
@@ -8,10 +10,18 @@ import (
 )
 
 func NewRedis(cfg *config.RedisConf) (*redis.Client, error) {
+	redis_URL, ok := os.LookupEnv("REDIS_TLS_URL")
+	if !ok {
+		log.Println("Couldn't get database url. Continuing with config")
+		redis_URL = cfg.Host + ":" + strconv.Itoa(cfg.Port)
+	} else {
+		log.Println("Redis url: ", redis_URL)
+	}
+
 	client := redis.NewClient(&redis.Options{
-		Addr: cfg.Host + ":" + strconv.Itoa(cfg.Port),
-		DB:   cfg.DB,
+		Addr: redis_URL,
 	})
+
 	_, err := client.Ping().Result()
 	if err != nil {
 		return nil, err
